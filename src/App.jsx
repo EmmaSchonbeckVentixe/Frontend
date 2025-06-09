b0a984d1860d7edff3ca954da46200220c9f52ee
Emma <emmaschonbeck7@gmail.com>
2025-06-09 08:34:04 +0200

    Initian commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Events from '../assets/pages/Events.jsx'
+import Bookings from '../assets/pages/Bookings.jsx'
+import EventDetails from '../assets/pages/EventDetails.jsx'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/events' element={<Events />} />
+      <Route path='/events/:id' element={<EventDetails />} />
+      <Route path='/bookings' element={<Bookings />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/assets/pages/Events.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const Events = () => {
+    const [events, setEvents] = useState([])
+
+  return (
+    <div>
+        {/* Lägg in kod för sidan (dela upp i komponenter dock) */}
+    </div>
+  )
+}
+
+export default Events
