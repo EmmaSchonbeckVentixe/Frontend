41abca86f92ca63608ada7badd01cfbaeea636fd
Emma <emmaschonbeck7@gmail.com>
2025-06-14 21:42:47 +0200

    Countinued with designing the eventlist page, added all necessary elements.

--- src/assets/components/EventItem.jsx
+++ src/assets/components/EventItem.jsx
@@ -3,12 +3,25 @@ import { Link } from 'react-router-dom'
 
 const EventItem = ({ item }) => {
     return (
-        <Link to={`/${item.id}`}>
+        <Link to={`/${item.id}`} className="event-link">
             <div className='event-card'>
                 <div>
                     <img className='img-eventcard' src="/Images/Eventlist/imgPlaceholder.jpeg" alt="Image placeholder" />
                 </div>
-                <div>{item.title}</div>
+                <div className='event-information'>
+                    <div className='event-date-time'>
+                        <p>June 5, 2029 - 3:00 PM</p>
+                    </div>
+                    <div className='event-title'>
+                        {item.title}
+                    </div>
+                    <div className='event-location'>
+                        <p>Rocky Ridge Exhibition Hall, Denver, CO</p>
+                    </div>
+                    <div className='event-price'>
+                        <p>40$</p>
+                    </div>
+                </div>
             </div>
         </Link>
     )
